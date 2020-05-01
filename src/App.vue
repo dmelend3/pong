7bfc8b6b3133977409b8f696b146c0b7ff3fa8be
dmelend3 <santos_9602@hotmail.com>
2020-05-01 12:03:50 -0500

    Main Layout created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+<v-app>
+  <v-app-bar app clipped-left dark color="red">
+    <v-toolbar-title>VuePong</v-toolbar-title>
+  </v-app-bar>
+  <v-navigation-drawer app clipped class="green lighten-1"> </v-navigation-drawer>
+    <v-content class="brown noOverflow">
+      <router-view />
+    </v-content>
+</v-app>
 </template>
